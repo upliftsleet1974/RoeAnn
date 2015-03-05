568e5d216261a7c73eb09ff3b10cb4d2346bf11f
upliftsleet1974 <upliftsleet1974@gmail.com>
2015-03-05 13:53:57 -0700

    First commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//var app = angular.module("lawnApp", ["firebase"]);
+
+//app.controller("LawnCtrl", function($scope, $firebase) {
+//  var ref = new Firebase("https://lawncoemail.firebaseio.com/");
+//  var sync = $firebase(ref);
+  // download the data into a local object
+//  var syncObject = sync.$asObject();
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+//  syncObject.$bindTo($scope, "site");
+//});
